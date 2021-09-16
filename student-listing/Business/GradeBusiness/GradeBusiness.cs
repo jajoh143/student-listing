aab73b1c0c019b18df7f4b9daf51e3f4328c47a1
Jacob Johnson <Jacob.Johnson@cpsi.com>
2021-09-16 14:11:22 -0500

    Added in methods to create and update courses and students

---
+++ student-listing/Business/GradeBusiness/GradeBusiness.cs
@@ -0,0 +1,37 @@
+﻿using student_listing.Data.DAL.GradeDataAccess;
+using student_listing.Data.DAL.RegistrationDataAccess;
+using student_listing.Data.DAL.StudentDataAccess;
+using student_listing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace student_listing.Business.GradeBusiness
+{
+    public class GradeBusiness : IGradeBusiness
+    {
+        /// <summary>
+        /// DAL Layer for student data
+        /// </summary>
+        private readonly IGradeDataAccess _gradeDataAccess;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="studentDataAccess"></param>
+        public GradeBusiness(IGradeDataAccess gradeDataAccess)
+        {
+            _gradeDataAccess = gradeDataAccess;
+        }
+
+        /// <summary>
+        /// Returns a list of all grades
+        /// </summary>
+        /// <returns>List of grades</returns>
+        public Task<IEnumerable<Grade>> GetGrades()
+        {
+            return _gradeDataAccess.GetGrades();
+        }
+    }
+}
